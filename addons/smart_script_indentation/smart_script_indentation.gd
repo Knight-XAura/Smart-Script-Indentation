8a7db0daef5f6ee75fc30a9000e68796f1a2055b
Knight-XAura <139917450+Knight-XAura@users.noreply.github.com>
2023-09-03 10:39:25 -0400

    Added Hot Key - Hard Coded

--- addons/smart_script_indentation/smart_script_indentation.gd
+++ addons/smart_script_indentation/smart_script_indentation.gd
@@ -40,6 +40,32 @@ func _process(delta: float) -> void:
 	script_code = script_editor.get_current_editor().get_base_editor() as CodeEdit
 	script_code.text_changed.connect(_on_text_changed)
 	set_process(false)
+	
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT):
+			var caret_position: int = script_code.get_caret_line()
+			for next_line in range(1, 4):
+				var line = script_code.get_line(caret_position + next_line).begins_with("func")
+				if line:
+					if next_line == 1:
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.set_caret_line(caret_position + 2)
+					elif next_line == 2:
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.set_caret_line(caret_position + 2)
+					elif next_line == 3:
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.set_caret_line(caret_position + 2)
+			delay_timer.stop()
+			enter_count = 0
 
 
 func _on_editor_settings_changed() -> void:
@@ -57,12 +83,31 @@ func _on_editor_script_changed(_script: Script) -> void:
 func _on_delay_timer_timeout() -> void:
 	enter_count = 0
 
+
 func _on_text_changed() -> void:
 	if Input.is_key_pressed(KEY_ENTER):
 		enter_count += 1
 		if enter_count == 1:
 			delay_timer.start()
 		elif enter_count >= 3:
-			script_code.set_line(script_code.get_caret_line(), "")
+			var caret_position: int = script_code.get_caret_line()
+			for next_line in range(1, 4):
+				var line = script_code.get_line(caret_position + next_line).begins_with("func")
+				if line:
+					if next_line == 1:
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.set_caret_line(caret_position + 2)
+					elif next_line == 2:
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.set_caret_line(caret_position + 2)
+					elif next_line == 3:
+						script_code.insert_line_at(caret_position, "")
+						script_code.insert_line_at(caret_position, "")
+						script_code.set_caret_line(caret_position + 2)
 			delay_timer.stop()
 			enter_count = 0
