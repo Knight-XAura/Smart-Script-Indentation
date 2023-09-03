9450c0e714ca1a105048badcc9644e4823aea388
Knight-XAura <139917450+Knight-XAura@users.noreply.github.com>
2023-09-03 13:56:37 -0400

    Fix always looking for func
    With this fix I also made some changes to the timing of constants to clean up the _process function. I also have allowed you to turn on/off find next func, but is not persistent

--- addons/smart_script_indentation/smart_script_indentation.gd
+++ addons/smart_script_indentation/smart_script_indentation.gd
@@ -13,7 +13,38 @@ var action_timer: Timer
 
 ### Settings ###
 
+const action_timeout_setting = {
+		"name": "Editor Plugins/Scripts/Smart Indent/Action Timeout",
+		"type": TYPE_FLOAT,
+		"hint": PROPERTY_HINT_RANGE,
+		"hint_string": "0, 3"
+	}
+const enter_count_threshold_setting = {
+		"name": "Editor Plugins/Scripts/Smart Indent/Enter Count Threshold",
+		"type": TYPE_INT,
+		"hint": PROPERTY_HINT_RANGE,
+		"hint_string": "1,4,1"
+	}
+const insert_line_spacing_hotkey_setting = {
+		"name": "Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey",
+		"type": TYPE_BOOL,
+		"hint": PROPERTY_HINT_NONE,
+		"hint_string": "PROPERTY_USAGE_CHECKABLE"
+	}
+const find_next_func_setting = {
+		"name": "Editor Plugins/Scripts/Smart Indent/Find Next Func",
+		"type": TYPE_BOOL,
+		"hint": PROPERTY_HINT_NONE,
+		"hint_string": "PROPERTY_USAGE_CHECKABLE"
+	}
+const find_next_func_threshold_setting = {
+		"name": "Editor Plugins/Scripts/Smart Indent/Find Next Func Threshold",
+		"type": TYPE_INT,
+		"hint": PROPERTY_HINT_RANGE,
+		"hint_string": "1,4,1"
+	}
 var enter_count: int
+var find_next_func: bool
 
 
 func _init() -> void:
@@ -30,47 +61,17 @@ func _process(delta: float) -> void:
 	editor_interface = get_editor_interface()
 	editor_settings = editor_interface.get_editor_settings()
 	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout", 1)
-	const action_timeout_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Action Timeout",
-		"type": TYPE_FLOAT,
-		"hint": PROPERTY_HINT_RANGE,
-		"hint_string": "0, 3"
-	}
 	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Enter Count Threshold", 3)
-	const enter_count_threshold_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Enter Count Threshold",
-		"type": TYPE_INT,
-		"hint": PROPERTY_HINT_RANGE,
-		"hint_string": "1,4,1"
-	}
 	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey", false)
-	const insert_line_spacing_hotkey_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey",
-		"type": TYPE_BOOL,
-		"hint": PROPERTY_HINT_NONE,
-		"hint_string": "PROPERTY_USAGE_CHECKABLE"
-	}
 	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Find Next Func", true)
-	const find_next_func_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Find Next Func",
-		"type": TYPE_BOOL,
-		"hint": PROPERTY_HINT_NONE,
-		"hint_string": "PROPERTY_USAGE_CHECKABLE"
-	}
 	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Find Next Func Threshold", 3)
-	const find_next_func_threshold_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Find Next Func Threshold",
-		"type": TYPE_INT,
-		"hint": PROPERTY_HINT_RANGE,
-		"hint_string": "1,4,1"
-	}
 	editor_settings.add_property_info(action_timeout_setting)
 	editor_settings.add_property_info(enter_count_threshold_setting)
 	editor_settings.add_property_info(insert_line_spacing_hotkey_setting)
 	editor_settings.add_property_info(find_next_func_setting)
 	editor_settings.add_property_info(find_next_func_threshold_setting)
 	editor_settings.settings_changed.connect(_on_editor_settings_changed)
-	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout") # How do I get this type safe?
+	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout")
 	script_editor = editor_interface.get_script_editor()
 	script_editor.editor_script_changed.connect(_on_editor_script_changed)
 	script_code = script_editor.get_current_editor().get_base_editor() as CodeEdit
@@ -80,26 +81,30 @@ func _process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT): # How do I get this type safe?
-			var caret_position: int = script_code.get_caret_line()
-			for next_line in range(1, 4):
-				var line = script_code.get_line(caret_position + next_line).begins_with("func")
-				if line:
-					if next_line == 1:
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.set_caret_line(caret_position + 2)
-					elif next_line == 2:
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.set_caret_line(caret_position + 2)
-					elif next_line == 3:
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.set_caret_line(caret_position + 2)
+		if event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT):
+			if find_next_func:
+				var caret_position: int = script_code.get_caret_line()
+				for next_line in range(1, 4):
+					var line_func = script_code.get_line(caret_position + next_line).begins_with("func")
+					var line_static_func = script_code.get_line(caret_position + next_line).begins_with("static func")
+					if line_func or line_static_func:
+						if next_line == 1:
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.set_caret_line(caret_position + 2)
+						elif next_line == 2:
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.set_caret_line(caret_position + 2)
+						elif next_line == 3:
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.set_caret_line(caret_position + 2)
+			else:
+				script_code.set_line(script_code.get_caret_line(), "")
 			action_timer.stop()
 			enter_count = 0
 
@@ -123,7 +128,8 @@ func _on_action_timer_timeout() -> void:
 func _on_editor_settings_changed() -> void:
 	action_timer.stop()
 	enter_count = 0
-	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout") # How do I get this type safe?
+	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout")
+	find_next_func = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Find_Next_Func")
 
 
 func _on_editor_script_changed(_script: Script) -> void:
@@ -140,24 +146,28 @@ func _on_text_changed() -> void:
 		if enter_count == 1:
 			action_timer.start()
 		elif enter_count >= 3:
-			var caret_position: int = script_code.get_caret_line()
-			for next_line in range(1, 4):
-				var line = script_code.get_line(caret_position + next_line).begins_with("func")
-				if line:
-					if next_line == 1:
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.set_caret_line(caret_position + 2)
-					elif next_line == 2:
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.set_caret_line(caret_position + 2)
-					elif next_line == 3:
-						script_code.insert_line_at(caret_position, "")
-						script_code.insert_line_at(caret_position, "")
-						script_code.set_caret_line(caret_position + 2)
+			if find_next_func:
+				var caret_position: int = script_code.get_caret_line()
+				for next_line in range(1, 4):
+					var line_func = script_code.get_line(caret_position + next_line).begins_with("func")
+					var line_static_func = script_code.get_line(caret_position + next_line).begins_with("static func")
+					if line_func or line_static_func:
+						if next_line == 1:
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.set_caret_line(caret_position + 2)
+						elif next_line == 2:
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.set_caret_line(caret_position + 2)
+						elif next_line == 3:
+							script_code.insert_line_at(caret_position, "")
+							script_code.insert_line_at(caret_position, "")
+							script_code.set_caret_line(caret_position + 2)
+			else:
+				script_code.set_line(script_code.get_caret_line(), "")
 			action_timer.stop()
 			enter_count = 0
