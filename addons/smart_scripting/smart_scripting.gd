8fe0737918cf080025ca91387c5fab6a0a86a179
Knight-XAura <139917450+Knight-XAura@users.noreply.github.com>
2023-09-04 19:00:25 -0400

    Updating Variable Verbiage in both environments

--- addons/smart_scripting/smart_scripting.gd
+++ addons/smart_scripting/smart_scripting.gd
@@ -14,37 +14,37 @@ var action_timer: Timer
 ### Settings ###
 
 const action_timeout_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Action Timeout",
+		"name": "Editor Plugins/Scripts/Smart Scripting/Action Timeout",
 		"type": TYPE_FLOAT,
 		"hint": PROPERTY_HINT_RANGE,
 		"hint_string": "0.33, 5"
 	}
-const new_line_count_threshold_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/New Line Count Threshold",
+const enter_count_threshold_setting = {
+		"name": "Editor Plugins/Scripts/Smart Scripting/Enter Count Threshold",
 		"type": TYPE_INT,
 		"hint": PROPERTY_HINT_RANGE,
 		"hint_string": "2,7,1"
 	}
-const insert_line_spacing_hotkey_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey",
+const insert_new_function_spacing_hotkey_setting = {
+		"name": "Editor Plugins/Scripts/Smart Scripting/Insert New Function Spacing Hotkey",
 		"type": TYPE_BOOL,
 		"hint": PROPERTY_HINT_NONE,
 		"hint_string": "PROPERTY_USAGE_CHECKABLE"
 	}
-const find_next_function_setting = {
-		"name": "Editor Plugins/Scripts/Smart Indent/Find Next Function",
+const find_upcoming_function_setting = {
+		"name": "Editor Plugins/Scripts/Smart Scripting/Find Upcoming Function",
 		"type": TYPE_BOOL,
 		"hint": PROPERTY_HINT_NONE,
 		"hint_string": "PROPERTY_USAGE_CHECKABLE"
 	}
-var new_line_count_threshold: int
-var hotkey: bool
-var find_next_function: bool
+var enter_count_threshold: int
+var insert_new_function_spacing_hotkey: bool
+var find_upcoming_function: bool
 
 
 ### Variables ###
 
-var new_line_count: int
+var enter_count: int
 const match_syntax: Array[String] = ["func ", "static func "]
 
 
@@ -66,19 +66,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	editor_interface = get_editor_interface()
 	editor_settings = editor_interface.get_editor_settings()
-	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout", 1)
-	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/New Line Count Threshold", 3)
-	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey", true) # Default: false
-	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Find Next Function", true)
+	editor_settings.set_setting("Editor Plugins/Scripts/Smart Scripting/Action Timeout", 1)
+	editor_settings.set_setting("Editor Plugins/Scripts/Smart Scripting/Enter Count Threshold", 3)
+	editor_settings.set_setting("Editor Plugins/Scripts/Smart Scripting/Insert New Function Spacing Hotkey", true) # Default: false
+	editor_settings.set_setting("Editor Plugins/Scripts/Smart Scripting/Find Upcoming Function", true)
 	editor_settings.add_property_info(action_timeout_setting)
-	editor_settings.add_property_info(new_line_count_threshold_setting)
-	editor_settings.add_property_info(insert_line_spacing_hotkey_setting)
-	editor_settings.add_property_info(find_next_function_setting)
+	editor_settings.add_property_info(enter_count_threshold_setting)
+	editor_settings.add_property_info(insert_new_function_spacing_hotkey_setting)
+	editor_settings.add_property_info(find_upcoming_function_setting)
 	editor_settings.settings_changed.connect(_on_editor_settings_changed)
-	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout")
-	new_line_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/New Line Count Threshold")
-	hotkey = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey")
-	find_next_function = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Find Next Function")
+	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Action Timeout")
+	enter_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Enter Count Threshold")
+	insert_new_function_spacing_hotkey = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Insert New Function Spacing Hotkey")
+	find_upcoming_function = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Find Upcoming Function")
 	script_editor = editor_interface.get_script_editor()
 	script_editor.editor_script_changed.connect(_on_editor_script_changed)
 	script_code = script_editor.get_current_editor().get_base_editor() as CodeEdit
@@ -88,25 +88,25 @@ func _process(delta: float) -> void:
 
 func _exit_tree() -> void: # Clean disconnects may not be needed? Similar to missing timer.queue_free()? RefCounted?
 	editor_settings.settings_changed.disconnect(_on_editor_settings_changed)
-	editor_settings.erase("Editor Plugins/Scripts/Smart Indent/Action Timeout")
-	editor_settings.erase("Editor Plugins/Scripts/Smart Indent/New Line Count Threshold")
-	editor_settings.erase("Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey")
-	editor_settings.erase("Editor Plugins/Scripts/Smart Indent/Find Next Function")
+	editor_settings.erase("Editor Plugins/Scripts/Smart Scripting/Action Timeout")
+	editor_settings.erase("Editor Plugins/Scripts/Smart Scripting/Enter Count Threshold")
+	editor_settings.erase("Editor Plugins/Scripts/Smart Scripting/Insert New Function Spacing Hotkey")
+	editor_settings.erase("Editor Plugins/Scripts/Smart Scripting/Find Upcoming Function")
 	script_editor.editor_script_changed.disconnect(_on_editor_script_changed)
 	script_code.gui_input.disconnect(_on_script_code_gui_input)
 	action_timer.timeout.disconnect(_on_action_timer_timeout)
 
 
 func _on_action_timer_timeout() -> void:
-	new_line_count = 0
+	enter_count = 0
 
 
 func _on_editor_settings_changed() -> void:
 	reset_action_data()
-	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout")
-	new_line_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/New Line Count Threshold")
-	hotkey = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey")
-	find_next_function = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Find Next Function")
+	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Action Timeout")
+	enter_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Enter Count Threshold")
+	insert_new_function_spacing_hotkey = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Insert New Function Spacing Hotkey")
+	find_upcoming_function = editor_settings.get_setting("Editor Plugins/Scripts/Smart Scripting/Find Upcoming Function")
 
 
 func _on_editor_script_changed(_script: Script) -> void:
@@ -118,70 +118,70 @@ func _on_editor_script_changed(_script: Script) -> void:
 
 func _on_script_code_gui_input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if hotkey and event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT):
+		if insert_new_function_spacing_hotkey and event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT):
 			reset_action_data()
 			var caret_line: int = script_code.get_caret_line()
-			if find_next_function:
-				for lines_to_match in new_line_count_threshold + 1:
+			if find_upcoming_function:
+				for lines_to_match in enter_count_threshold + 1:
 					for syntax in match_syntax:
 						if script_code.get_line(caret_line + lines_to_match).begins_with(syntax):
 							if lines_to_match == 0:
-								for line_space in new_line_count_threshold - 1:
+								for line_space in enter_count_threshold - 1:
 									script_code.insert_line_at(caret_line, "")
-								for line_space in new_line_count_threshold:
+								for line_space in enter_count_threshold:
 									script_code.insert_line_at(caret_line, "")
 								script_code.set_caret_line(caret_line + 2)
 								return
 							caret_line += 1
-							for line_space in new_line_count_threshold:
+							for line_space in enter_count_threshold:
 								script_code.insert_line_at(caret_line, "")
 							script_code.set_caret_line(caret_line)
-							for line_space in new_line_count_threshold - 1:
+							for line_space in enter_count_threshold - 1:
 								script_code.insert_line_at(caret_line, "")
 							return
 			var caret_position = script_code.get_caret_column()
 			var line_length = script_code.get_line(caret_line).length()
 			if caret_position != line_length:
-				for needed_spacing in new_line_count_threshold * 2 - 1:
+				for needed_spacing in enter_count_threshold * 2 - 1:
 					script_code.insert_line_at(caret_line, "")
-				script_code.set_caret_line(caret_line + new_line_count_threshold - 1)
+				script_code.set_caret_line(caret_line + enter_count_threshold - 1)
 			elif caret_position == line_length:
-				for needed_spacing in new_line_count_threshold * 2 - 1:
+				for needed_spacing in enter_count_threshold * 2 - 1:
 					script_code.insert_line_at(caret_line + 1, "")
-				script_code.set_caret_line(caret_line + new_line_count_threshold)
+				script_code.set_caret_line(caret_line + enter_count_threshold)
 		elif Input.is_key_pressed(KEY_ENTER):
-			new_line_count += 1
-		if new_line_count == 1:
+			enter_count += 1
+		if enter_count == 1:
 			action_timer.start()
-		elif new_line_count == new_line_count_threshold:
+		elif enter_count == enter_count_threshold:
 			reset_action_data()
 			var caret_line: int = script_code.get_caret_line()
-			if find_next_function:
-				for lines_to_match in new_line_count_threshold + 1:
+			if find_upcoming_function:
+				for lines_to_match in enter_count_threshold + 1:
 					for syntax in match_syntax:
 						if script_code.get_line(caret_line + lines_to_match).begins_with(syntax):
 							if lines_to_match == 0:
-								for line_space in new_line_count_threshold - 1:
+								for line_space in enter_count_threshold - 1:
 									script_code.insert_line_at(caret_line, "")
 								script_code.set_caret_line(caret_line - 1)
 								return
-							for line_space in new_line_count_threshold - lines_to_match:
+							for line_space in enter_count_threshold - lines_to_match:
 								script_code.insert_line_at(caret_line + 1, "")
 							script_code.set_line(caret_line, "")
 							return
 			var caret_position = script_code.get_caret_column()
 			var line_length = script_code.get_line(caret_line).length()
 			if caret_position != line_length:
-				for needed_spacing in new_line_count_threshold - 1:
+				for needed_spacing in enter_count_threshold - 1:
 					script_code.insert_line_at(caret_line, "")
 				script_code.set_caret_line(caret_line - 1)
 				script_code.set_line(caret_line - 1, "")
 			elif caret_position == line_length:
-				for needed_spacing in new_line_count_threshold - 1:
+				for needed_spacing in enter_count_threshold - 1:
 					script_code.insert_line_at(caret_line + 1, "")
 				script_code.set_line(caret_line, "")
 
 
 func reset_action_data() -> void:
 	action_timer.stop()
-	new_line_count = 0
+	enter_count = 0
--- smart_scripting_test/smart_scripting_test.gd
+++ smart_scripting_test/smart_scripting_test.gd
@@ -7,23 +7,23 @@ var script_code: CodeEdit
 var action_timer: Timer
 
 @onready var action_tiemout_slider: HSlider = $VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer1/ActionTiemout
-@onready var new_line_count_threshold_spinbox: SpinBox = $VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/NewLineCountThreshold
-@onready var hot_key_checkbox: CheckBox = $VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/HotKey
-@onready var find_next_function_checkbox: CheckBox = $VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/FindNextFunction
+@onready var enter_count_threshold_spinbox: SpinBox = $VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/EnterCountThreshold
+@onready var insert_new_function_spacing_hotkey_checkbox: CheckBox = $VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/InsertNewFunctionSpacingHotkey
+@onready var find_upcoming_function_checkbox: CheckBox = $VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer2/FindUpcomingFunction
 
 
 ### Settings ###
 
-@export_range(2, 7, 1) var new_line_count_threshold: int = 3
-@export var hotkey: bool = true
-@export var find_next_function: bool = true
+@export_range(2, 7, 1) var enter_count_threshold: int = 3
+@export var insert_new_function_spacing_hotkey: bool = true
+@export var find_upcoming_function: bool = true
 
 @export_range(0.33, 5) var action_timeout: float = 1
 
 
 ### Variables ###
 
-var new_line_count: int
+var enter_count: int
 const match_syntax: Array[String] = ["func ", "static func "]
 
 
@@ -55,76 +55,76 @@ func _exit_tree() -> void: # Clean disconnects may not be needed? Similar to mis
 
 
 func _on_action_timer_timeout() -> void:
-	new_line_count = 0
+	enter_count = 0
 
 
 func reset_action_data() -> void:
 	action_timer.stop()
-	new_line_count = 0
+	enter_count = 0
 
 
 func _on_script_code_gui_input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if hotkey and event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT):
+		if insert_new_function_spacing_hotkey and event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT):
 			reset_action_data()
 			var caret_line: int = script_code.get_caret_line()
-			if find_next_function:
-				for lines_to_match in new_line_count_threshold + 1:
+			if find_upcoming_function:
+				for lines_to_match in enter_count_threshold + 1:
 					for syntax in match_syntax:
 						if script_code.get_line(caret_line + lines_to_match).begins_with(syntax):
 							if lines_to_match == 0:
-								for line_space in new_line_count_threshold - 1:
+								for line_space in enter_count_threshold - 1:
 									script_code.insert_line_at(caret_line, "")
-								for line_space in new_line_count_threshold:
+								for line_space in enter_count_threshold:
 									script_code.insert_line_at(caret_line, "")
 								script_code.set_caret_line(caret_line + 2)
 								return
 							caret_line += 1
-							for line_space in new_line_count_threshold:
+							for line_space in enter_count_threshold:
 								script_code.insert_line_at(caret_line, "")
 							script_code.set_caret_line(caret_line)
-							for line_space in new_line_count_threshold - 1:
+							for line_space in enter_count_threshold - 1:
 								script_code.insert_line_at(caret_line, "")
 							return
 			var caret_position = script_code.get_caret_column()
 			var line_length = script_code.get_line(caret_line).length()
 			if caret_position != line_length:
-				for needed_spacing in new_line_count_threshold * 2 - 1:
+				for needed_spacing in enter_count_threshold * 2 - 1:
 					script_code.insert_line_at(caret_line, "")
-				script_code.set_caret_line(caret_line + new_line_count_threshold - 1)
+				script_code.set_caret_line(caret_line + enter_count_threshold - 1)
 			elif caret_position == line_length:
-				for needed_spacing in new_line_count_threshold * 2 - 1:
+				for needed_spacing in enter_count_threshold * 2 - 1:
 					script_code.insert_line_at(caret_line + 1, "")
-				script_code.set_caret_line(caret_line + new_line_count_threshold)
+				script_code.set_caret_line(caret_line + enter_count_threshold)
 		elif Input.is_key_pressed(KEY_ENTER):
-			new_line_count += 1
-		if new_line_count == 1:
+			enter_count += 1
+		if enter_count == 1:
 			action_timer.start()
-		elif new_line_count == new_line_count_threshold:
+		elif enter_count == enter_count_threshold:
 			reset_action_data()
 			var caret_line: int = script_code.get_caret_line()
-			if find_next_function:
-				for lines_to_match in new_line_count_threshold + 1:
+			if find_upcoming_function:
+				for lines_to_match in enter_count_threshold + 1:
 					for syntax in match_syntax:
 						if script_code.get_line(caret_line + lines_to_match).begins_with(syntax):
 							if lines_to_match == 0:
-								for line_space in new_line_count_threshold - 1:
+								for line_space in enter_count_threshold - 1:
 									script_code.insert_line_at(caret_line, "")
 								script_code.set_caret_line(caret_line - 1)
 								return
-							for line_space in new_line_count_threshold - lines_to_match:
+							for line_space in enter_count_threshold - lines_to_match:
 								script_code.insert_line_at(caret_line + 1, "")
 							script_code.set_line(caret_line, "")
 							return
 			var caret_position = script_code.get_caret_column()
 			var line_length = script_code.get_line(caret_line).length()
 			if caret_position != line_length:
-				for needed_spacing in new_line_count_threshold - 1:
+				for needed_spacing in enter_count_threshold - 1:
 					script_code.insert_line_at(caret_line, "")
 				script_code.set_caret_line(caret_line - 1)
 				script_code.set_line(caret_line - 1, "")
 			elif caret_position == line_length:
-				for needed_spacing in new_line_count_threshold - 1:
+				for needed_spacing in enter_count_threshold - 1:
 					script_code.insert_line_at(caret_line + 1, "")
 				script_code.set_line(caret_line, "")
 
@@ -134,13 +134,13 @@ func _on_action_tiemout_drag_ended(_value_changed: bool) -> void:
 	action_timer.wait_time = action_timeout
 
 
-func _on_new_line_count_threshold_value_changed(value: int) -> void:
-	new_line_count_threshold = value
+func _on_enter_count_threshold_value_changed(value: int) -> void:
+	enter_count_threshold = value
 
 
-func _on_hot_key_toggled(button_pressed: bool) -> void:
-	hotkey = button_pressed
+func _on_insert_new_function_spacing_hotkey_toggled(button_pressed: bool) -> void:
+	insert_new_function_spacing_hotkey = button_pressed
 
 
-func _on_find_next_function_toggled(button_pressed: bool) -> void:
-	find_next_function = button_pressed
+func _on_find_upcoming_function_toggled(button_pressed: bool) -> void:
+	find_upcoming_function = button_pressed
