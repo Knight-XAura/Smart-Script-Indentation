6b9a9acd1b21997e675377fbe70b46a4f1539a93
Knight-XAura <139917450+Knight-XAura@users.noreply.github.com>
2023-09-03 23:54:18 -0400

    finished fixing most formatting, mostly dynamic - nonpersistent
    Verbiage changes for variable with enter in name

--- addons/smart_script_indentation/smart_script_indentation.gd
+++ addons/smart_script_indentation/smart_script_indentation.gd
@@ -19,7 +19,7 @@ const action_timeout_setting = {
 		"hint": PROPERTY_HINT_RANGE,
 		"hint_string": "0, 3"
 	}
-const enter_count_threshold_setting = {
+const new_line_count_threshold_setting = {
 		"name": "Editor Plugins/Scripts/Smart Indent/Enter Count Threshold",
 		"type": TYPE_INT,
 		"hint": PROPERTY_HINT_RANGE,
@@ -43,14 +43,14 @@ const find_next_func_threshold_setting = {
 		"hint": PROPERTY_HINT_RANGE,
 		"hint_string": "1,4,1"
 	}
-var enter_count_threshold: int
+var new_line_count_threshold: int
 var hotkey: bool
 var find_next_func: bool
 
 
 ### Variables ###
 
-var enter_count: int
+var new_line_count: int
 const match_syntax: Array[String] = ["func ", "static func "]
 
 
@@ -74,13 +74,13 @@ func _process(delta: float) -> void:
 	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Find Next Func", true)
 	editor_settings.set_setting("Editor Plugins/Scripts/Smart Indent/Find Next Func Threshold", 3)
 	editor_settings.add_property_info(action_timeout_setting)
-	editor_settings.add_property_info(enter_count_threshold_setting)
+	editor_settings.add_property_info(new_line_count_threshold_setting)
 	editor_settings.add_property_info(insert_line_spacing_hotkey_setting)
 	editor_settings.add_property_info(find_next_func_setting)
 	editor_settings.add_property_info(find_next_func_threshold_setting)
 	editor_settings.settings_changed.connect(_on_editor_settings_changed)
 	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout")
-	enter_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Enter Count Threshold")
+	new_line_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Enter Count Threshold")
 	hotkey = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey")
 	find_next_func = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Find Next Func")
 	script_editor = editor_interface.get_script_editor()
@@ -94,65 +94,39 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if hotkey and event.is_pressed() and event.keycode == KEY_ENTER and Input.is_key_pressed(KEY_SHIFT):
 			var caret_line: int = script_code.get_caret_line()
-			for lines_to_match in enter_count_threshold + 1:
+			for lines_to_match in new_line_count_threshold + 1:
 				for syntax in match_syntax:
 					if find_next_func and script_code.get_line(caret_line + lines_to_match).begins_with(syntax):
 						if lines_to_match == 0:
-							for line_space in enter_count_threshold - 1:
+							for line_space in new_line_count_threshold - 1:
 								script_code.insert_line_at(caret_line, "")
-							for line_space in enter_count_threshold:
+							for line_space in new_line_count_threshold:
 								script_code.insert_line_at(caret_line, "")
-							script_code.set_caret_line(caret_line - enter_count_threshold)
+							script_code.set_caret_line(caret_line + 2)
 							reset_temp_data()
 							return
-						for line_space in enter_count_threshold - lines_to_match:
+						print(lines_to_match)
+						caret_line += 1
+						for line_space in new_line_count_threshold:
 							script_code.insert_line_at(caret_line, "")
-						for line_space in enter_count_threshold - lines_to_match:
-							script_code.insert_line_at(caret_line + 1, "")
-						script_code.set_line(caret_line, "")
+						script_code.set_caret_line(caret_line)
+						for line_space in new_line_count_threshold - 1:
+							script_code.insert_line_at(caret_line, "")
 						reset_temp_data()
 						return
-			print("NO FUNC")
 			var caret_position = script_code.get_caret_column()
 			var line_length = script_code.get_line(caret_line).length()
 			if caret_position != line_length:
-				for needed_spacing in enter_count_threshold * 2 - 1:
+				for needed_spacing in new_line_count_threshold * 2 - 1:
 					script_code.insert_line_at(caret_line, "")
-				script_code.set_caret_line(caret_line + enter_count_threshold - 1)
+				script_code.set_caret_line(caret_line + new_line_count_threshold - 1)
 			elif caret_position == line_length:
-				for needed_spacing in enter_count_threshold * 2 - 1:
+				for needed_spacing in new_line_count_threshold * 2 - 1:
 					script_code.insert_line_at(caret_line + 1, "")
-				script_code.set_caret_line(caret_line + enter_count_threshold)
+				script_code.set_caret_line(caret_line + new_line_count_threshold)
 			reset_temp_data()
 			return
-#			var caret_line: int = script_code.get_caret_line()
-#			for lines_to_match in enter_count_threshold + 1:
-#				for syntax in match_syntax:
-#					if find_next_func and script_code.get_line(caret_line + lines_to_match).begins_with(syntax):
-#						if lines_to_match == 0:
-#							script_code.insert_line_at(caret_line, "")
-#							script_code.insert_line_at(caret_line, "")
-#							script_code.insert_line_at(caret_line, "")
-#							script_code.insert_line_at(caret_line, "")
-#							script_code.insert_line_at(caret_line, "")
-#							script_code.set_caret_line(caret_line + 2)
-#						elif lines_to_match == 1:
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.set_caret_line(caret_line + 2)
-#						elif lines_to_match == 2:
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.set_caret_line(caret_line + 2)
-#						elif lines_to_match == 3:
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.insert_line_at(caret_line + 1, "")
-#							script_code.set_caret_line(caret_line + 2)
-#					reset_temp_data()
-#					return
+
 
 func _exit_tree() -> void:
 	editor_settings.settings_changed.disconnect(_on_editor_settings_changed)
@@ -167,13 +141,13 @@ func _exit_tree() -> void:
 
 
 func _on_action_timer_timeout() -> void:
-	enter_count = 0
+	new_line_count = 0
 
 
 func _on_editor_settings_changed() -> void:
 	reset_temp_data()
 	action_timer.wait_time = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Action Timeout")
-	enter_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Enter Count Threshold")
+	new_line_count_threshold = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Enter Count Threshold")
 	hotkey = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Insert Line Spacing Hotkey")
 	find_next_func = editor_settings.get_setting("Editor Plugins/Scripts/Smart Indent/Find Next Func")
 
@@ -187,21 +161,21 @@ func _on_editor_script_changed(_script: Script) -> void:
 
 func _on_script_code_text_changed() -> void:
 	if Input.is_key_pressed(KEY_ENTER):
-		enter_count += 1
-		if enter_count == 1:
+		new_line_count += 1
+		if new_line_count == 1:
 			action_timer.start()
-		elif enter_count == enter_count_threshold:
+		elif new_line_count == new_line_count_threshold:
 			var caret_line: int = script_code.get_caret_line()
-			for lines_to_match in enter_count_threshold + 1:
+			for lines_to_match in new_line_count_threshold + 1:
 				for syntax in match_syntax:
 					if find_next_func and script_code.get_line(caret_line + lines_to_match).begins_with(syntax):
 						if lines_to_match == 0:
-							for line_space in enter_count_threshold - 1:
+							for line_space in new_line_count_threshold - 1:
 								script_code.insert_line_at(caret_line, "")
 							script_code.set_caret_line(caret_line - 1)
 							reset_temp_data()
 							return
-						for line_space in enter_count_threshold - lines_to_match:
+						for line_space in new_line_count_threshold - lines_to_match:
 							script_code.insert_line_at(caret_line + 1, "")
 						script_code.set_line(caret_line, "")
 						reset_temp_data()
@@ -209,12 +183,12 @@ func _on_script_code_text_changed() -> void:
 			var caret_position = script_code.get_caret_column()
 			var line_length = script_code.get_line(caret_line).length()
 			if caret_position != line_length:
-				for needed_spacing in enter_count_threshold - 1:
+				for needed_spacing in new_line_count_threshold - 1:
 					script_code.insert_line_at(caret_line, "")
 				script_code.set_caret_line(caret_line - 1)
 				script_code.set_line(caret_line - 1, "")
 			elif caret_position == line_length:
-				for needed_spacing in enter_count_threshold - 1:
+				for needed_spacing in new_line_count_threshold - 1:
 					script_code.insert_line_at(caret_line + 1, "")
 				script_code.set_line(caret_line, "")
 			reset_temp_data()
@@ -223,4 +197,4 @@ func _on_script_code_text_changed() -> void:
 
 func reset_temp_data() -> void:
 	action_timer.stop()
-	enter_count = 0
+	new_line_count = 0
